1e8f17cd8a2557c0f31f3ecac95f94b8f2efcdc6
mmadia <mmadia@b174e083-3eed-4968-8d8e-c8f379eb2ae9>
2009-03-27 06:22:33 +0000

    Relicensed files as MIT with permission from Werner Freytag. TODO : create Jamfile
    git-svn-id: http://svn.osdrawer.net/clipup@4 b174e083-3eed-4968-8d8e-c8f379eb2ae9

--- Input Device/InputDevice.h
+++ Input Device/InputDevice.h
@@ -1,3 +1,13 @@
+/*
+ * Copyrights (c):
+ *     2001 - 2008 , Werner Freytag.
+ *     2009, Haiku
+ * Distributed under the terms of the MIT License.
+ *
+ * Original Author:
+ *              Werner Freytag <freytag@gmx.de>
+ */
+
 #ifndef _CLIPUP_INPUT_DEVICE_H
 #define _CLIPUP_INPUT_DEVICE_H
 
